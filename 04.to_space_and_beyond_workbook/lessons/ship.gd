96ca9415988c8793cad2d5f243c8fad14b33123b
mel0327 <meho@s.sfusd.edu>
2024-03-18 11:53:24 -0700

    L5 and practice

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -5,9 +5,10 @@ var normal_speed := 600.0
 
 var max_speed := normal_speed
 var velocity := Vector2(0, 0)
+var steering_factor := 10.0
 
 func _process(delta: float) -> void:
-	var direction := Vector2(0,0)
+	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 
@@ -18,7 +19,9 @@ func _process(delta: float) -> void:
 		max_speed = boost_speed
 		get_node("Timer").start()
 
-	velocity = direction * max_speed
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 
 	if direction.length() > 0.0:
