12789f5a98beed3c983cf6ecc0b309fb4b8663a6
mel0327 <meho@s.sfusd.edu>
2024-04-02 11:02:50 -0700

    L5 and Practice

--- 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
+++ 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
@@ -22,6 +22,9 @@ func _process(delta: float) -> void:
 	if velocity.length() > 0.0:
 		get_node("Sprite2D").rotation = velocity.angle()
 
-
+func set_energy(new_energy: int)-> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
+	
 func _on_area_entered(area: Area2D) -> void:
-	pass
+	set_energy(energy + 20)
